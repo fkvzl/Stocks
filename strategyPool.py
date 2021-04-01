ec66aaab228b39e66f40e16d7357efbb1dba2656
1274243237@qq.com <1274243237@qq.com>
2021-04-01 20:38:50 +0800

    talib完成验证
    完成talibe的macd验证

--- strategyPool.py
+++ strategyPool.py
@@ -25,7 +25,7 @@ db='stockdb'
 engine = create_engine(f'mysql+pymysql://{user}:{pwd}@{ip}:3306/{db}')
 
 
-
+#指标
 def myMACD(price, fastperiod, slowperiod, signalperiod):
     ewma12 = price.ewm(span=fastperiod,adjust=False).mean()
     ewma60 = price.ewm(span=slowperiod,adjust=False).mean()
@@ -34,17 +34,22 @@ def myMACD(price, fastperiod, slowperiod, signalperiod):
     bar = (dif-dea)*2 #有些地方的bar = (dif-dea)*2，但是talib中MACD的计算是bar = (dif-dea)*1
     return dif,dea,bar
 
-def getLongLow(price,sday,eday):
+
+#选股
+#hist在1.21开始没跌破0
+#def getLongLow(price,sday,eday):
     #设置开始、截止时间
     #设置下跌幅度
     #筛选发生过满足以上条件的股票，观察走势，比如价格低于17年，macd是红的超过了1个月
+    
 
-df = pro.daily(ts_code='300910.SZ',start_date=20201127,end_date=20210329)#遍历每天行情
-df = df[::-1]
+df = pro.daily(ts_code='002269.SZ')#遍历每天行情
+df=df[::-1]  #倒序，同sort区别为sort为排序方式
 
-dif,dea,hist=myMACD(df['close'],fastperiod=12, slowperiod=26, signalperiod=9)
-plt.plot(df['trade_date'].values,dif,label='dif')
-plt.plot(df['trade_date'].values,dea,label='dea')
-plt.plot(df['trade_date'].values,hist,label='hist')
-plt.legend(loc='best')
-print (dif)
+#以下两种方式效果一致，talib无前33位，区域需要全周期
+#dif,dea,hist=myMACD(df['close'],fastperiod=12, slowperiod=26, signalperiod=9)
+dif,dea,hist=tl.MACD(df['close'],fastperiod=12, slowperiod=26, signalperiod=9)
+
+
+hist = hist*2
+print(hist)
