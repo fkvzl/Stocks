5e1932e7731ca2c0ae73a31964a7bed96ecf732a
fkvzl <1274243237@qq.com>
2021-05-19 23:18:40 +0800

    完成创业板涨跌数量矩阵
    画图还未解决

--- dataView.py
+++ dataView.py
@@ -11,11 +11,31 @@ E-mail:fkvzl@qq.com
 
 """
 import xcsc_tushare as ts
+from matplotlib import pyplot as plt
+import numpy as np
 
+#初始化
 ts.set_token('db359948bb4351fe9731151b3ad7925b240419250d16094af141acd5')
 pro = ts.pro_api(env='prd')
-df=pro.daily(trade_date=20210512)
-df_cyb=df[df['ts_code'].str.contains('^300')]
-cyb_rises=(df_cyb[df_cyb['pct_chg']>0].count())
-cyb_fails=(df_cyb[df_cyb['pct_chg']<0].count())
-print(flat)
+
+
+#横坐标x：交易日期
+tra_cal = pro.trade_cal(start_date=20210101,end_date=20210519)
+x = tra_cal.trade_date.values
+
+#纵坐标：每天的涨跌数量
+yr = []
+yf = []
+for i in x:
+    df=pro.daily(trade_date=i)
+        dfcyb_r=df[df.ts_code.str.contains('^300')& df.pct_chg>0]
+    dfcyb_f=df[df.ts_code.str.contains('^300')& df.pct_chg<0]
+    cyb_rises=dfcyb_r['pct_chg'].count()
+    cyb_fails=dfcyb_f['pct_chg'].count()
+    yr.append(cyb_rises)   
+    yf.append(cyb_fails)
+    
+#画图
+plt.figure()
+plt.plot(x,yr)
+plt.show()
