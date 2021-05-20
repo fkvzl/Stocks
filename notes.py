c2cd57be9b52ba10963cf1d7488c0e8f4a932df3
1274243237@qq.com <1274243237@qq.com>
2021-05-20 08:06:45 +0800

    文档追加

---
+++ notes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 14 08:20:13 2021
+
+@author: fkvzl
+
+E-mail:fkvzl@qq.com
+
+主题：python笔记
+思路：
+
+"""
+【基础语法】
+1、dataframe.count()：用来统计每行/每列的数量，精确级别
+2、loc为向下移动
+3、df['K']表示新增一列K字段
+4、
+【技术指标】
+macd跟kdj要自己写，talib中默认前33数值为NA
